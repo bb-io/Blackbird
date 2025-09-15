19877664d37cc594b6119f24010be9248ba164e5
RiabushenkoA <ariabushenko@blackbird.io>
2025-09-15 21:28:07 +0300

    Added test project

---
+++ Tests.Blackbird/Base/FileManager.cs
@@ -0,0 +1,49 @@
+﻿using Blackbird.Applications.Sdk.Common.Files;
+using Blackbird.Applications.SDK.Extensions.FileManagement.Interfaces;
+
+namespace Tests.Blackbird
+{
+    public class FileManager : IFileManagementClient
+    {
+        public readonly string inputFolder;
+        public readonly string outputFolder;
+
+        public FileManager(string folderLocation)
+        {
+            var baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            var projectDirectory = Directory.GetParent(baseDirectory).Parent.Parent.Parent.FullName;
+
+
+            var testFilesPath = Path.Combine(projectDirectory, "TestFiles");
+            inputFolder = Path.Combine(folderLocation, "Input");
+            outputFolder = Path.Combine(folderLocation, "Output");
+
+            Directory.CreateDirectory(inputFolder);
+            Directory.CreateDirectory(outputFolder);
+        }
+
+
+        public Task<Stream> DownloadAsync(FileReference reference)
+        {
+            var path = Path.Combine(inputFolder, reference.Name);
+            Assert.IsTrue(File.Exists(path), $"File not found at: {path}");
+            var bytes = File.ReadAllBytes(path);
+
+            var stream = new MemoryStream(bytes);
+            return Task.FromResult((Stream)stream);
+        }
+
+        public Task<FileReference> UploadAsync(Stream stream, string contentType, string fileName)
+        {
+            var path = Path.Combine(outputFolder, fileName);
+            new FileInfo(path).Directory.Create();
+            using (var fileStream = File.Create(path))
+            {
+                stream.CopyTo(fileStream);
+            }
+
+            return Task.FromResult(new FileReference() { Name = fileName, ContentType=contentType });
+        }
+
+    }
+}
---
+++ Tests.Blackbird/Validator.cs
@@ -0,0 +1,30 @@
+using Apps.Blackbird.Connections;
+using Blackbird.Applications.Sdk.Common.Authentication;
+
+namespace Tests.Blackbird
+{
+    [TestClass]
+    public class Validator : TestBase
+    {
+        [TestMethod]
+        public async Task ValidatesCorrectConnection()
+        {
+            var validator = new ConnectionValidator();
+
+            var result = await validator.ValidateConnection(Creds, CancellationToken.None);
+            Console.WriteLine(result.Message);
+            Assert.IsTrue(result.IsValid);
+        }
+
+        [TestMethod]
+        public async Task DoesNotValidateIncorrectConnection()
+        {
+            var validator = new ConnectionValidator();
+
+            var newCreds = Creds.Select(x => new AuthenticationCredentialsProvider(x.KeyName, x.Value + "_incorrect"));
+            var result = await validator.ValidateConnection(newCreds, CancellationToken.None);
+            Console.WriteLine(result.Message);
+            Assert.IsFalse(result.IsValid);
+        }
+    }
+}
