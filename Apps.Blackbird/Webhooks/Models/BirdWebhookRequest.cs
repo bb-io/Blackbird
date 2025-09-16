1d1cd16c86f1461ad1d6fe44bc5a64781704d7f8
RiabushenkoA <ariabushenko@blackbird.io>
2025-09-16 10:30:36 +0300

    Updated webhook

--- Apps.Blackbird/Webhooks/Models/BirdWebhookRequest.cs
+++ Apps.Blackbird/Webhooks/Models/BirdWebhookRequest.cs
@@ -1,5 +1,7 @@
 ﻿using Apps.Blackbird.DataSourceHandlers;
+using Apps.Blackbird.DataSourceHandlers.Static;
 using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Dictionaries;
 using Blackbird.Applications.Sdk.Common.Dynamic;
 
 namespace Apps.Blackbird.Webhooks.Models
@@ -13,5 +15,9 @@ namespace Apps.Blackbird.Webhooks.Models
         [Display("Bird ID")]
         [DataSource(typeof(BirdDataSourceHandler))]
         public string? BirdId { get; set; }
+
+        [Display("Bird status")]
+        [StaticDataSource(typeof(BirdStatusDataHandler))]
+        public string? BirdStatus { get; set; }
     }
 }
