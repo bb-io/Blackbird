a8428da43f4628a7ef1f9abf97214a0db6bb32ff
Anton Fesenko <tohafesenkolovep@gmail.com>
2025-11-20 15:17:53 +0200

    update BlackbirdAppClient.ConfigureErrorException, add NestActionsTests

---
+++ Tests.Blackbird/NestActionsTests.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using Apps.Blackbird.Actions;
+using Apps.Blackbird.Models.Request.Nests;
+
+namespace Tests.Blackbird;
+
+[TestClass]
+public class NestActionsTests : TestBase
+{
+    [TestMethod]
+    public async Task ListNests_ReturnsNests()
+    {
+		// Arrange
+		var actions = new NestActions(InvocationContext);
+
+        // Act
+        var result = await actions.ListNests();
+
+        // Assert
+        Console.WriteLine(JsonConvert.SerializeObject(result, Formatting.Indented));
+        Assert.IsNotNull(result);
+    }
+
+    [TestMethod]
+    public async Task GetNest_ReturnsNest()
+    {
+        // Arrange
+        var actions = new NestActions(InvocationContext);
+        var request = new NestRequest { NestId = "687" };
+
+        // Act
+        var result = await actions.GetNest(request);
+
+        // Assert
+        Console.WriteLine(JsonConvert.SerializeObject(result, Formatting.Indented));
+        Assert.IsNotNull(result);
+    }
+}
